f652c05c5ccef340ed14646ba0fb0ff840382672
Pavan Kaushik <pavan@nice.ncbs.res.in>
2016-02-29 19:39:30 +0530

    deleted:    height64.png 	new file:   models/green.tga 	new file:   models/model-inf.dat 	new file:   models/red.tga 	new file:   models/sphere.egg 	deleted:    world0.bam 	deleted:    world64.bam 	modified:   world_0.3.py

--- world_0.3.py
+++ world_0.3.py
@@ -7,65 +7,114 @@ from panda3d.core import AmbientLight, DirectionalLight, Vec4, Vec3, Fog, LVecBa
 from panda3d.core import loadPrcFileData, NodePath
 from pandac.PandaModules import CompassEffect
 from direct.gui.OnscreenText import OnscreenText
+
 size=257
 class MyApp(ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)
-        rospy.init_node('apple')  # record stats # initialize mouse #init node
-        loadPrcFileData("", "win-size 2720 768")
-        self.debug=True
+        loadPrcFileData("", "win-size 2720 768")                        #set window size
+        ShowBase.__init__(self)                                         #start the app
+        rospy.init_node('apple')                                        #init node
+        self.debug=True                                                 #deubbing, prints pos
         self.statusLabel=self.makeStatusLabel(0)
 
-        self.world = self.loader.loadModel("models/world"+str(size)+".bam")
-        self.world.reparentTo(self.render)
+        self.world = self.loader.loadModel("models/world"+str(size)+".bam") #loads the world of dimesions size
+        self.world.reparentTo(self.render)                              #render the world
 
         # relevant for world boundaries
-        self.worldsize = size
-
-        # self.player = self.loader.loadModel("models/alliedflanker.egg")
+        self.worldSize = size
+        self.initPos=LVecBase3f(self.worldSize/2,self.worldSize/2.5,1.3)  #z is factored by 2 dunno why?
+        self.initH=0
+        #the Player
         self.player=NodePath("player")
-        self.initPos=LVecBase3f(self.worldsize/2,self.worldsize/2,1.2)
         self.player.setPos(self.world,self.initPos)
-        self.player.setH(self.world,0)
+        self.player.setH(self.world,self.initH)                                  #heading angle is 0
 
 
         self.speed = 0.0
         self.maxspeed = 25.0
         self.speedIncrement=0.1
 
+        #spheres
+
+        #Red sphere
+        #load model, set position, set scale, apply texture
+        self.redSphere=self.loader.loadModel("models/sphere.egg")
+        self.redSphere.setPos(self.world,self.worldSize/2-10,self.worldSize/2,1.3)
+        self.redSphere.setScale(0.25)
+        #load redSphere texture
+        self.redTex=self.loader.loadTexture("models/red.tga")
+        self.redSphere.setTexture(self.redTex)
+        self.redSphere.reparentTo(self.render)
+
+        #Green Sphere
+        self.greenSphere=self.loader.loadModel("models/sphere.egg")
+        self.greenSphere.setPos(self.redSphere,10,0,0)
+        self.greenSphere.setScale(0.25)
+        #load greenSphere texture
+        self.greenTex=self.loader.loadTexture("models/green.tga")
+        self.greenSphere.setTexture(self.greenTex)
+        # self.greenSphere.reparentTo(self.render)
+
+        #Load tree
+        self.tree=self.loader.loadModel("models/treeHighB.egg")
+        self.tree.setPos(self.world,self.worldSize/2+10,self.worldSize/2,1)
+        self.tree.setScale(0.01)
+
+        self.treeTex=self.loader.loadTexture("models/BarkBrown.tga")
+        self.tree.setTexture(self.treeTex)
+        self.tree.reparentTo(self.render)
+
+        #wbad
+        self.wbad = self.wbas = 0
+        self.closedGain=1 #gain for turning
+        self.listener()
+
         # A task to run every frame, some keyboard setup and our speed
         self.taskMgr.add(self.updateTask, "update")
         self.keyboardSetup()
 
         # performance (to be masked later by fog) and view:
-        self.maxdistance = 100
+        self.maxdistance = 200
         self.camLens.setFar(self.maxdistance)
-        self.camLens.setFov(90)
+        self.camLens.setFov(120)
         self.createEnvironment()
 
+
+    def callback(self, data):
+        """ Returns Wing Beat Amplitude Difference from received data"""
+        self.wbad = data.left.angles[0] - data.right.angles[0]
+        self.wbas = data.left.angles[0] + data.right.angles[0]
+        return self.wbad
+
+    def listener(self):
+        """ Listens to Kinefly Flystate topic"""
+        rospy.Subscriber("/kinefly/flystate", MsgFlystate, self.callback)
+        # print self.wbad
     def makeStatusLabel(self,i):
         return OnscreenText(style=2,fg=(0.5,1,.5,1),pos=(-1.3,0.92-(.08*i)),mayChange=1)
+
     def keyboardSetup(self):
         self.keyMap = {"left":0, "right":0, "climb":0, "fall":0, \
                               "accelerate":0, "decelerate":0, "closed":0,"reverse":0,"init":0}
         self.accept("escape", sys.exit)
-        self.accept("a", self.setKey, ["accelerate",1])
-        self.accept("a-up", self.setKey, ["accelerate",0])
-        self.accept("z", self.setKey, ["decelerate",1])
-        self.accept("z-up", self.setKey, ["decelerate",0])
+        self.accept("a", self.setKey, ["climb",1])
+        self.accept("a-up", self.setKey, ["climb",0])
+        self.accept("z", self.setKey, ["fall",1])
+        self.accept("z-up", self.setKey, ["fall",0])
         self.accept("arrow_left", self.setKey, ["left",1])
         self.accept("arrow_left-up", self.setKey, ["left",0])
         self.accept("arrow_right", self.setKey, ["right",1])
         self.accept("arrow_right-up", self.setKey, ["right",0])
-        self.accept("arrow_down", self.setKey, ["climb",1])
-        self.accept("arrow_down-up", self.setKey, ["climb",0])
-        self.accept("arrow_up", self.setKey, ["fall",1])
-        self.accept("arrow_up-up", self.setKey, ["fall",0])
+        self.accept("arrow_down", self.setKey, ["decelerate",1])
+        self.accept("arrow_down-up", self.setKey, ["decelerate",0])
+        self.accept("arrow_up", self.setKey, ["accelerate",1])
+        self.accept("arrow_up-up", self.setKey, ["accelerate",0])
         self.accept("o", self.setKey, ["closed",0])
         self.accept("p", self.setKey, ["closed",1])
         self.accept("r",self.setKey,["reverse",1])
         self.accept("r-up",self.setKey,["reverse",0])
         self.accept("i",self.setKey,["init",1])
+        self.accept("i-up",self.setKey,["init",0])
         base.disableMouse() # or updateCamera will fail!
 
     def createEnvironment(self):
@@ -95,7 +144,6 @@ class MyApp(ShowBase):
         render.setLight(render.attachNewNode(ambientLight))
         render.setLight(render.attachNewNode(directionalLight))
 
-
     def setKey(self, key, value):
         self.keyMap[key] = value
 
@@ -111,6 +159,10 @@ class MyApp(ShowBase):
         climbfactor = (.01)+scalefactor * 1
         bankfactor  = (.4)+scalefactor  *6.5
         speedfactor = scalefactor *1
+
+        #closed loop
+        if (self.keyMap["closed"] != 0):
+            self.player.setH(self.player.getH() + self.wbad*self.closedGain)
 
         # Climb and Fall
         if (self.keyMap["climb"]!=0):# and self.speed > 0.00):
@@ -136,31 +188,36 @@ class MyApp(ShowBase):
             if (self.speed < 0.0):
                 self.speed = 0.0
 
-        # move forwards - our X/Y is inverted, see the issue
+        # reverse gear
+        if (self.keyMap["reverse"]!=0):
+            self.speed-=self.speedIncrement
+
+        # move forwards
         self.player.setY(self.player,speedfactor)
 
         # respect max camera distance else you
         # cannot see the floor post loop the loop!
         if (self.player.getZ() > self.maxdistance):
             self.player.setZ(self.maxdistance)
-        # should never happen once we add collision, but in case:
+
         elif (self.player.getZ() < 0.5):
             self.player.setZ(0.5)
 
         # and now the X/Y world boundaries:
         if (self.player.getX() < 0):
             self.player.setX(0)
-        elif (self.player.getX() > self.worldsize):
-            self.player.setX(self.worldsize)
+        elif (self.player.getX() > self.worldSize):
+            self.player.setX(self.worldSize)
 
         if (self.player.getY() < 0):
             self.player.setY(0)
-        elif (self.player.getY() > self.worldsize):
-            self.player.setY(self.worldsize)
+        elif (self.player.getY() > self.worldSize):
+            self.player.setY(self.worldSize)
 
         #reset to initial position
         if (self.keyMap["init"]!=0):
-            self.player.setPos(self.initPos)
+            self.player.setPos(self.world,self.initPos)
+            self.player.setH(self.initH)
 
 
 
@@ -170,7 +227,8 @@ class MyApp(ShowBase):
         # see issue content for how we calculated these:
         self.camera.setPos(self.player, 0,0,0)#25.6225, 3.8807, 10.2779)
         self.camera.setHpr(self.player,0,0,0)#94.8996,-16.6549,1.55508)
-        print "POS"+str(self.player.getPos())
-        print "HPR"+str(self.player.getHpr())
+        if self.debug:
+            print "POS"+str(self.player.getPos(self.world))
+            print "HPR"+str(self.player.getHpr(self.world))
 app = MyApp()
 app.run()
