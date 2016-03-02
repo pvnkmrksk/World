3e4bfd1612350b9e36e1541800a68f3f8e5d204e
Pavan Kaushik <pavan@nice.ncbs.res.in>
2016-03-02 21:37:54 +0530

    add real time trajectory, add handBreak, autoStart kinefly and roscore

--- world_0.3.py
+++ world_0.3.py
@@ -1,4 +1,6 @@
 import sys
+import time
+
 import rospy
 # import servo
 from beginner.msg import MsgFlystate
@@ -8,12 +10,32 @@ from direct.task import Task
 from panda3d.core import AmbientLight, DirectionalLight, Vec4, Vec3, Fog, LVecBase3f
 from panda3d.core import loadPrcFileData, NodePath
 from pandac.PandaModules import CompassEffect
+import subprocess, rostopic
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
+import numpy as np
+
+try:
+    # Checkif rosmaster is running or not.
+    rostopic.get_topic_class('/rosout')
+    is_rosmaster_running = True
+except rostopic.ROSTopicIOException as e:
+    roscore = subprocess.Popen('roscore')  # then start roscore yourself
+    time.sleep(1)  # wait a bit to be sure the roscore is really launched
+    subprocess.Popen(["roslaunch", "Kinefly", "main.launch"])
+    # os.system("roslaunch Kinefly main.launch")
+    # time.sleep(1)
 
 size = 257
+pos = ori = []
+
+print "pos is ", pos
 
 
 class MyApp(ShowBase):
     def __init__(self):
+        global treePos, redPos, prevPos, ax, fig
         loadPrcFileData("", "win-size 2720 768")  # set window size
         ShowBase.__init__(self)  # start the app
         rospy.init_node('apple')  # init node
@@ -29,15 +51,16 @@ class MyApp(ShowBase):
 
         # params
         self.worldSize = size  # relevant for world boundaries
-        self.initPos = LVecBase3f(self.worldSize / 2, 114, 1.3)  # z is factored by 2 dunno why?
+        self.initPos = LVecBase3f(self.worldSize / 2, 90, 1.3)  # z is factored by 2 dunno why?
         self.initH = 0
-
-
+        prevPos = (self.initPos[0], self.initPos[1])
+        treePos = Vec3(self.worldSize / 2 + 20, self.worldSize / 2, 1)
+        redPos = Vec3(self.worldSize / 2 - 20, self.worldSize / 2, 1.8)
         self.speed = 0.0
         self.maxspeed = 25.0
         self.speedIncrement = 0.1
 
-        self.closedGain = 0.1  # gain for turning
+        self.closedGain = 1  # gain for turning
 
         self.maxdistance = 200
         self.camLens.setFar(self.maxdistance)
@@ -60,27 +83,27 @@ class MyApp(ShowBase):
         if self.loadSpheres:
             # Red sphere
             self.redSphere = self.loader.loadModel("models/sphere.egg")
-            self.redSphere.setPos(self.world, self.worldSize / 2 - 10, self.worldSize / 2, 1.8)
+            self.redSphere.setPos(self.world, redPos)
             self.redSphere.setScale(0.4)
             # load redSphere texture
             self.redTex = self.loader.loadTexture("models/red.tga")
             self.redSphere.setTexture(self.redTex)
-            # self.redSphere.reparentTo(self.render)
+            self.redSphere.reparentTo(self.render)
 
             # Green Sphere
             self.greenSphere = self.loader.loadModel("models/sphere.egg")
-            self.greenSphere.setPos(self.world, self.worldSize / 2 +10, self.worldSize / 2, 1.8)
+            self.greenSphere.setPos(self.world, self.worldSize / 2 - 20, self.worldSize / 2, 1.8)
             self.greenSphere.setScale(0.4)
             # load greenSphere texture
             self.greenTex = self.loader.loadTexture("models/green.tga")
             self.greenSphere.setTexture(self.greenTex)
-            self.greenSphere.reparentTo(self.render)
+            # self.greenSphere.reparentTo(self.render)
 
         # Load tree
 
         if self.loadTree:
             self.tree = self.loader.loadModel("models/treeHighB.egg")
-            self.tree.setPos(self.world, self.worldSize / 2 - 10, self.worldSize / 2, 1)
+            self.tree.setPos(self.world, treePos)
             self.tree.setScale(0.03)
 
             self.treeTex = self.loader.loadTexture("models/BarkBrown.tga")
@@ -95,13 +118,31 @@ class MyApp(ShowBase):
         self.taskMgr.add(self.updateTask, "update")
         self.keyboardSetup()
         self.createEnvironment()
+
+        fig = plt.figure()
+        self.trajectory()
+        self.frameNum = 0
+
         # self.i
 
     def trajectory(self):
-        pass
 
-    def windTunnel(self,windDirection):
-        self.servoAngle=(self.player.getH()%360)-180
+        global ax
+        ax=fig.add_subplot(111)
+        ax.set_xlim(0,size)
+        ax.set_ylim(0,size)
+        plt.plot(treePos[0],treePos[1],'gs')
+        plt.plot(redPos[0],redPos[1],'ro')
+        plt.ion()
+        plt.show()
+        # # global pos, ori
+        # # pos.append(self.player.getPos(self.world))
+        # # ori.append(self.player.getHpr(self.world))
+        # # # print "pos is ",pos
+        # # print "ori is ",ori
+
+    def windTunnel(self, windDirection):
+        self.servoAngle = (self.player.getH() % 360) - 180
 
     def modelLoader(self, modelName, modelPath, modelPos, modelScale, texName, texPath, modelParent):
         modelName = self.loader.loadModel(modelPath)
@@ -129,9 +170,9 @@ class MyApp(ShowBase):
 
     def keyboardSetup(self):
         self.keyMap = {"left": 0, "right": 0, "climb": 0, "fall": 0, \
-                       "accelerate": 0, "decelerate": 0, "reverse": 0,
+                       "accelerate": 0, "decelerate": 0, "handBrake": 0, "reverse": 0,
                        "closed": 0, "init": 0, "gain-up": 0, "gain-down": 0,
-                       "newInit": 0}
+                       "newInit": 0, "clf": 0}
         self.accept("escape", sys.exit)
         self.accept("a", self.setKey, ["climb", 1])
         self.accept("a-up", self.setKey, ["climb", 0])
@@ -149,6 +190,8 @@ class MyApp(ShowBase):
         self.accept("p", self.setKey, ["closed", 1])
         self.accept("r", self.setKey, ["reverse", 1])
         self.accept("r-up", self.setKey, ["reverse", 0])
+        self.accept("s", self.setKey, ["handBrake", 1])
+        self.accept("s-up", self.setKey, ["handBrake", 0])
         self.accept("i", self.setKey, ["init", 1])
         self.accept("i-up", self.setKey, ["init", 0])
         self.accept("u", self.setKey, ["gain-up", 1])
@@ -157,7 +200,8 @@ class MyApp(ShowBase):
         self.accept("y-up", self.setKey, ["gain-down", 0])
         self.accept("]", self.setKey, ["newInit", 1])
         self.accept("]-up", self.setKey, ["newInit", 0])
-
+        self.accept("q", self.setKey, ["clf", 1])
+        self.accept("q-up", self.setKey, ["clf", 0])
         base.disableMouse()  # or updateCamera will fail!
 
     def createEnvironment(self):
@@ -196,12 +240,33 @@ class MyApp(ShowBase):
         return Task.cont
 
     def updatePlayer(self):
+        global prevPos, currentPos, ax, fig, treePos, redPos
         # Global Clock
         # by default, panda runs as fast as it can frame to frame
         scalefactor = self.speed * 1 / 250  # (globalClock.getDt()*self.speed)
         climbfactor = (.01) + scalefactor * 1
         bankfactor = .5  # (.4) + scalefactor * 6.5
         speedfactor = scalefactor * 1
+
+        self.frameNum+=1
+        if (self.frameNum%30==1):
+
+            currentPos=(self.player.getX(),self.player.getY())
+            verts=[(prevPos),(currentPos)]
+            codes=[Path.MOVETO,Path.LINETO]
+            path=Path(verts,codes)
+
+            patch=patches.PathPatch(path,facecolor='white',lw=2)#,color=hex(self.frameNum))
+            ax.add_patch(patch)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+            prevPos=currentPos
+
+
+
+        if (self.keyMap["clf"] != 0):
+            plt.clf()
+            self.trajectory()
 
         # print self.wbad
 
@@ -232,6 +297,9 @@ class MyApp(ShowBase):
             self.speed -= self.speedIncrement
             if (self.speed < 0.0):
                 self.speed = 0.0
+        # handbrake
+        if (self.keyMap["handBrake"] != 0):
+            self.speed = 0
 
         # reverse gear
         if (self.keyMap["reverse"] != 0):
@@ -264,9 +332,9 @@ class MyApp(ShowBase):
             self.player.setPos(self.world, self.initPos)
             self.player.setH(self.initH)
 
-        if (self.keyMap["newInit"]!=0):
-            self.initPos=self.player.getPos(self.world)
-            self.initH=self.player.getH(self.world)
+        if (self.keyMap["newInit"] != 0):
+            self.initPos = self.player.getPos(self.world)
+            self.initH = self.player.getH(self.world)
             print "new init pos is ", self.initPos
             print "new init H is ", self.initH
         # update gain
@@ -286,7 +354,5 @@ class MyApp(ShowBase):
         if self.debug:
             print "POS" + str(self.player.getPos(self.world))
             print "HPR" + str(self.player.getHpr(self.world))
-
-
 app = MyApp()
 app.run()
