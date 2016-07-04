3fb06aa224b35d08ff16623d2c6f2f2338165e52
Pavan Kaushik <pavan@nice.ncbs.res.in>
2016-07-04 14:03:14 +0530

    Add odourfield generator. The valve is controlled by the odour matrix add stripGen. Creates a strip of odour plume.
    	deleted:    models/alliedflanker.egg
    	deleted:    models/alliedflanker.jpg
    	deleted:    models/world_size:257_obj:0r_num:10x10_lattice:20.bam
    	deleted:    models/world_size:257_obj:0r_num:10x10_lattice:25.bam
    	deleted:    models/world_size:257_obj:0r_num:1x1_lattice:20.bam
    	deleted:    models/world_size:257_obj:0t_num:10x10_lattice:25.bam
    	deleted:    models/world_size:257_obj:0t_num:10x15_lattice:25.bam
    	deleted:    models/world_size:257_obj:gr_num:10x10_lattice:20.bam
    	deleted:    models/world_size:257_obj:gr_num:10x10_lattice:25.bam
    	deleted:    models/world_size:257_obj:r0_num:10x10_lattice:25.bam
    	deleted:    models/world_size:257_obj:r0_num:10x15_lattice:25.bam
    	deleted:    models/world_size:257_obj:rg_num:10x15_lattice:25.bam
    	modified:   params.py
    	modified:   servo.py
    	modified:   world.py

--- params.py
+++ params.py
@@ -3,21 +3,42 @@ from panda3d.core import Vec3
 parameters = {
     # bools
 
+    "loadWind": True,
+    "loadOdour": True,
+
+    "fly": "fly6",
+    "race": "colony apple",
+    "emergence": 20160507,
+    "age": 50,
+    "windQuad": [-1, -1, 180, 180],  # -1 is open loop
+    "odourQuad":[0,1,0,1], #0 is always off, 1 is always on, c is custom
+    "loadingString": "rg",
+    "trialNo": 1,
+
+    "generateWorld": False,
+    "gain": 7.0,
+    "maxSpeed": 3.5,
+
     "loadHUD": False,
     "loadTrajectory": True,
-    "lockFps": True,  # False,
-    "frameRecord": False,
-    "loadWind":  False,
-    "disabledFly": False,
-    "generateWorld": True,
     "loadWorld": True,
-    "replayWorld": False,
+    "disabledFly": False,
 
+    "frameRecord": False,
+    "replayWorld": False,
     "captureScale": 1,  # scale the window to improve speed
     "captureStart": 0,  # start point of playback
     "playbackIncrement": 1,  # step size of playback
     "recordDur": 10,  # in seconds
     "recordFps": 30,
+
+    "spherePath": "models/sphere.egg",
+    "greenTexPath": "models/green.tga",
+    # "greenTexPath": "models/greenRed.tga",
+    # "redTexPath": "models/greenRed.tga",
+    "redTexPath": "models/red.tga",
+    "treePath": "models/treeHighB.egg",
+    "treeTexPath": "models/BarkBrown.tga",
 
     "hcp": False,
     "quad": True,
@@ -25,16 +46,6 @@ parameters = {
     "posR": (96, 64),
     "bboxDist": 5.5,
     "maxBoutDur": 36000,  # frames
-
-    "fly": "fly6",
-    "race": "colony apple",
-    "emergence": 20160507,
-    "age": 50,
-    "windQuad": [-1, 180, 225, 135],  # -1 is open loop
-    "loadingString": "rg",
-    "gain": 7.0,
-    "maxSpeed": 3.5,
-    "trialNo": 1,
 
     "modelSizeSuffix": "257",
     "modelHeightMap": "models/height" + "257" + "0.png",
@@ -52,7 +63,7 @@ parameters = {
     "origin": (0, 0, 0),
 
     "lrGain": 1.0,
-
+    "lockFps": True,  # False,
     "fps": 165,
     "trajectoryUpdateInterval": 165,  # frames between update,
 
@@ -82,19 +93,8 @@ parameters = {
 
     "windowWidth": 4320,  # 1440,
     "windowHeight": 2560,
-
-    "spherePath": "models/sphere.egg",
-    "greenTexPath": "models/green.tga",
-    # "greenTexPath": "models/greenRed.tga",
-    # "redTexPath": "models/greenRed.tga",
-    "redTexPath": "models/red.tga",
-    "treePath": "models/treeHighB.egg",
-    "treeTexPath": "models/BarkBrown.tga",
     "bagTopics": "/usb_cam/image_raw /kinefly/image_output /kinefly/flystate /trajectory",
 
 }
 
 
-
-# for key, val in parameters.items():
-#     print key+"="+"parameters[\""+key.replace('self.','').strip(" ")+"\"]"
