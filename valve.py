70724fb6f334b61cd07c372fda307a193fc18f6e
Pavan Kaushik <pavan@nice.ncbs.res.in>
2016-07-06 12:12:50 +0530

    fix weird bug in valve. Int conversion of raw input in a separate step else no valve control

--- valve.py
+++ valve.py
@@ -2,8 +2,11 @@ import servo
 while True:
     try:
         print "Enter the valve state 0=off, 1=on"
-        valveState=int(raw_input())
-        servo.move(99, valveState) #99 is the valve case
+        valveState=(raw_input())
+        servoAngle=int(valveState)
+        servo.move(99,servoAngle)#99 is the valve case
+
+
     except KeyboardInterrupt:
         print " thanks for controlling the valve"
         break
