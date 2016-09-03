c45796037ab671a16d1f702a36c69c4943c85614
Pavan Kaushik <pavan@nice.ncbs.res.in>
2016-09-03 20:21:13 +0530

    refactor bagControl as a new class, OOP FTW fix traj bag not having complete metadata json the metadata incase of disaster

--- world.py
+++ world.py
@@ -1191,111 +1191,21 @@ class MyApp(ShowBase):
         self.cameraRight.setPos(self.player, 0, 0, 0)
         self.cameraRight.setH(self.player, 240)  # self.world, self.player.getH())#-120)
 
-
     # recording functions
     def bagControl(self):
         if (self.keyMap["startBag"] == 1):
-            self.startBag()
+            self.trajBagger=BagControl('traj',parameters['bagTrajTopics'])
+            self.fullBagger=BagControl('full',parameters['bagFullTopics'])
+
+            self.trial = 0
+            self.frame = 0
+            self.bagRecordingState = True
 
         elif (self.keyMap["stopBag"] != 0):
-            self.stopbag()
+            self.trajBagger.stopbag()
+            self.fullBagger.stopbag()
 
-    def startBag(self):
-        self.frame=0
-        self.bagRecordingState = True
-        self.trial = 0
-
-        self.bagger("traj")
-        self.bagger("full")
-
-        obj=self.metadataGen()
-        self.pickler(obj, self.bagFilename)
-
-        time.sleep(0.15)
-
-    def stopbag(self):
-        # self.runBagCommand.send_signal(subprocess.signal.SIGINT) #send signal on stop command
-        self.terminate_ros_node("/record")
-        self.bagRecordingState = False
-        rospy.loginfo("\n \n \n Bag recording stopped \n \n \n ")
-        self.addMetadata()
-        print "metadata added \n \n "
-        print "\n \n bagfilename is", self.bagFilename
-
-    def bagger(self,bagType):
-        self.bagFilenameGen(bagType)
-        if bagType=="traj":
-            topics=parameters["bagTrajTopics"]
-        elif bagType=="full":
-            topics=parameters["bagTopics"]
-        self.bagCommand = "rosbag record --lz4 --output-name=" + self.bagFilename + " " \
-                          + topics
-        # print self.bagCommand
-        self.runBagCommand = subprocess.Popen(self.bagCommand, shell=True, stdout=subprocess.PIPE)
-        rospy.loginfo("Bag recording started")
-
-    def bagFilenameGen(self,bagType):
-        self.timeNow = str(datetime.now().strftime('%Y-%m-%d__%H:%M:%S'))
-        if parameters["hcp"]:
-            mode = "hcp_"
-        elif parameters["quad"]:
-            if parameters["loadWind"]:
-                mode = "wind_quad_"
-            else:
-                mode = "quad_"
-
-        bagDir="bags/"+str(datetime.now().strftime('%Y_%m_%d'))#make a directory of current date
-        if not os.path.exists(bagDir):
-            os.makedirs(bagDir)#create dir if non existent
-
-        self.bagFilename = bagDir+"/" + self.timeNow+"_"+parameters["fly"] + "_" + mode + parameters["loadingString"] \
-                           + "_gain" + str(parameters["gain"])  + "_trial_" + str(parameters["trialNo"])+ "_"+bagType
-
-        print self.bagFilename
-    def terminate_ros_node(self, s):
-        list_cmd = subprocess.Popen("rosnode list", shell=True, stdout=subprocess.PIPE)
-        list_output = list_cmd.stdout.read()
-        retcode = list_cmd.wait()
-        assert retcode == 0, "List command returned %d" % retcode
-        for str in list_output.split("\n"):
-            if (str.startswith(s)):
-                os.system("rosnode kill " + str)
-
-    def metadataGen(self):
-        file = open(__file__, 'r')
-        servo = open("servo.py", 'r')
-        arduino = open("servoControl/servoControl.ino", 'r')
-        bam= open(self.worldFilename)
-        obj= dict(parameters=parameters,world=file.read(),
-                  servo=servo.read(),arduino=arduino.read(),)
-                  #bam=bam.read().encode('utf-8').strip())
-        # obj = [parameters, file.read(), servo.read(), arduino.read(), world.read()]
-        return obj
-
-    def addMetadata(self):
-        print "allo " + self.bagFilename
-        a = self.bagFilename + ".bag"
-        time.sleep(5)  # so that bag file can be transfereed from memory
-
-        obj=self.metadataGen()
-        # a.encode('utf-8').strip()
-        # obj=[122]
-        # print obj
-        metadata = (json.dumps(obj))
-        metadata=String(metadata)
-        # print "metadata is:", metadata
-
-        with rosbag.Bag(a, 'a') as bag:
-            i = 0
-            for _, _, t in bag.read_messages():
-                if i == 0:
-                    tstamp = t
-                i += 1
-                break
-            bag.write('/metadata', metadata, tstamp)
-
-
-            # datasave
+            self.bagRecordingState = False
 
     def pickler(self, obj, path):
         """
@@ -1479,6 +1389,105 @@ class MyApp(ShowBase):
             return maxn
         else:
             return n
+
+
+class BagControl():
+    def __init__(self, bagType, topics):
+        self.bagType=bagType
+        self.topics=topics
+        self.startBag()
+
+    def startBag(self):
+        self.bagger()
+        obj = self.metadataGen()
+        app.pickler(obj, self.bagFilename)
+
+        with open(self.bagFilename+".json", 'w') as outfile:
+            json.dump(obj, outfile, indent=4, sort_keys=True, separators=(',', ':'))
+        time.sleep(0.15)  # sleep to prevent multiple instatntiations for a single keypress
+
+    def stopbag(self):
+        # self.runBagCommand.send_signal(subprocess.signal.SIGINT) #send signal on stop command
+        self.terminate_ros_node("/record")
+        rospy.loginfo("\n \n \n Bag recording stopped \n \n \n ")
+        self.addMetadata()
+        print "metadata added \n \n "
+        print "\n \n bagfilename is", self.bagFilename
+
+    def bagger(self):
+        self.bagFilename = self.bagFilenameGen()
+        self.bagCommand = "rosbag record --lz4 --output-name=" + self.bagFilename + " " \
+                          + self.topics
+        # print self.bagCommand
+        self.runBagCommand = subprocess.Popen(self.bagCommand, shell=True, stdout=subprocess.PIPE)
+        rospy.loginfo("Bag recording started")
+
+    def bagFilenameGen(self):
+        self.timeNow = str(datetime.now().strftime('%Y-%m-%d__%H:%M:%S'))
+        mode = ""
+        if parameters["hcp"]:
+            mode += "hcp_"
+        if parameters["loadWind"]:
+            mode += "wind_"
+        if parameters["imposeStimulus"]:
+            mode += "impose_"
+        if parameters["quad"]:
+            mode += "quad_"
+
+        bagDir = "bags/" + str(datetime.now().strftime('%Y_%m_%d'))  # make a directory of current date
+        if not os.path.exists(bagDir):
+            os.makedirs(bagDir)  # create dir if non existent
+
+        fileName = bagDir + "/" + self.timeNow + "_" + parameters["fly"] + "_" \
+                   + mode + parameters["loadingString"] \
+                   + "_gain" + str(parameters["gain"]) \
+                   + "_trial_" + str(parameters["trialNo"]) + "_" + self.bagType
+        print fileName
+        return fileName
+
+
+
+    def addMetadata(self):
+        print "allo " + self.bagFilename
+        bagName = self.bagFilename + ".bag"
+        if self.bagType=='full':
+            time.sleep(5)  # so that bag file can be transfereed from memory
+
+        obj = self.metadataGen()
+        metadata = (json.dumps(obj))
+        metadata = String(metadata)
+        # print "metadata is:", metadata
+
+        with rosbag.Bag(bagName, 'a') as bag:
+            i = 0
+            for _, _, t in bag.read_messages():
+                if i == 0:
+                    tstamp = t
+                i += 1
+                break
+            bag.write('/metadata', metadata, tstamp)
+            # datasave
+
+    def metadataGen(self):
+        file = open(__file__, 'r')
+        servo = open("servo.py", 'r')
+        arduino = open("servoControl/servoControl.ino", 'r')
+        bam = open(app.worldFilename)
+        obj = dict(parameters=parameters, world=file.read(),
+                   servo=servo.read(), arduino=arduino.read(), )
+        # bam=bam.read().encode('utf-8').strip())
+        # obj = [parameters, file.read(), servo.read(), arduino.read(), world.read()]
+        return obj
+
+
+    def terminate_ros_node(self, s):
+        list_cmd = subprocess.Popen("rosnode list", shell=True, stdout=subprocess.PIPE)
+        list_output = list_cmd.stdout.read()
+        retcode = list_cmd.wait()
+        assert retcode == 0, "List command returned %d" % retcode
+        for str in list_output.split("\n"):
+            if (str.startswith(s)):
+                os.system("rosnode kill " + str)
 
 
 app = MyApp()
