23478c32f332b3f1a923450e8156c4b23031fa0a
Marian Renz <marianrenz@web.de>
2016-11-02 21:30:10 +0530

    Fix save as bug, implement circ method, start rebasing worldGen.py and model system

--- worldGen.py
+++ worldGen.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json_tricks as json
 import sys
+import math
 
 from helping.helper import paramsFromGUI
 parameters=paramsFromGUI()
@@ -43,15 +44,26 @@ class WorldGen(ShowBase):  # our 'class'
             ShowBase.__init__(self)  # initialise
 
             self.initTerrain()
-            self.initModels()
-            self.initPositions()
-            self.loadingStringParser(parameters["loadingString"])
+            self.quadPositionGenerator()
+            #self.initModels()
+            self.getObjects()
+            #self.initPositions()
+            self.setObjPositions()
 
-            # self.plotPosition()
-            if self.evenLoad:
-                self.evenInstance(self.evenObj, scale=self.evenScale, tex=self.evenTex,z=self.evenZ)
-            if self.oddLoad:
-                self.oddInstance(self.oddObj, scale=self.oddScale, tex=self.oddTex,z=self.oddZ)
+            # if parameters["loadingString"] == 'circ':
+            #     # self.setObjects()
+            #     for i in range(0,360,30):
+            #         self.setCircPos(dummy=self.redSphere, scale=parameters['sphereScale'], z=3, teta=i, r=50, tex=self.redTex)
+            #     print "circ successfull"
+            # else:
+            #
+            #     self.loadingStringParser(parameters["loadingString"])
+            #
+            # # self.plotPosition()
+            #     if self.evenLoad:
+            #        self.evenInstance(self.evenObj, scale=self.evenScale, tex=self.evenTex,z=self.evenZ)
+            #     if self.oddLoad:
+            #        self.oddInstance(self.oddObj, scale=self.oddScale, tex=self.oddTex,z=self.oddZ)
 
             # print " \n \n \n Generate is about to exit \n \n \n"
 
@@ -87,15 +99,15 @@ class WorldGen(ShowBase):  # our 'class'
         self.root.reparentTo(self.render)  # render from root
         self.root.setSz(0.2)  # maximum height
         self.terrain.generate()  # generate
-
-    def initModels(self):
-        self.tree = self.loader.loadModel(parameters["treePath"])
-        self.greenSphere = self.loader.loadModel(parameters["spherePath"])
-        self.redSphere = self.loader.loadModel(parameters["spherePath"])
-
-        self.treeTex = self.loader.loadTexture(parameters["treeTexPath"])
-        self.greenTex = self.loader.loadTexture(parameters["greenTexPath"])
-        self.redTex = self.loader.loadTexture(parameters["redTexPath"])
+    #
+    # def initModels(self):
+    #     self.tree = self.loader.loadModel(parameters["treePath"])
+    #     self.greenSphere = self.loader.loadModel(parameters["spherePath"])
+    #     self.redSphere = self.loader.loadModel(parameters["spherePath"])
+    #
+    #     self.treeTex = self.loader.loadTexture(parameters["treeTexPath"])
+    #     self.greenTex = self.loader.loadTexture(parameters["greenTexPath"])
+    #     self.redTex = self.loader.loadTexture(parameters["redTexPath"])
 
 
     def initPositions(self):
@@ -105,7 +117,7 @@ class WorldGen(ShowBase):  # our 'class'
 
         if parameters["quad"]:
 
-            self.odd,self.even=self.quadPositionGenerator()
+            self.odd,self.even=self.quadPositionGenerator()#todo: ???
 
         # print "odd is",self.odd
         # print "even is ", self.even
@@ -139,113 +151,187 @@ class WorldGen(ShowBase):  # our 'class'
         print "even is ", even
         return odd,even
 
-    def loadingStringParser(self, loadingString):
-        """
-        Args:
-            loadingString:2 letter string,
-            r - red sphere
-            g - green sphere
-            t - tree
-            0 - None
+    # def loadingStringParser(self, loadingString):
+    #     """
+    #     Args:
+    #         loadingString:2 letter string,
+    #         r - red sphere
+    #         g - green sphere
+    #         t - tree
+    #         0 - None
+    #
+    #         1st letter is even instance
+    #         2nd letter is odd instance
+    #
+    #     Returns:
+    #
+    #     """
+    #     if len(loadingString) > 2:
+    #         print "only 2 objects in world"
+    #
+    #     j = 0
+    #     for i in loadingString:
+    #
+    #         if i == "t":
+    #             obj = self.tree
+    #             tex = self.treeTex
+    #             scale = parameters["treeScale"]
+    #             z=parameters["treeZ"]
+    #
+    #             plotColor="k"
+    #             plotMarker="s"
+    #             load = True
+    #
+    #         elif i == "r":
+    #             obj = self.redSphere
+    #             tex = self.redTex
+    #             scale = parameters["sphereScale"]
+    #             z=parameters["sphereZ"]
+    #
+    #             plotColor="r"
+    #             plotMarker="o"
+    #             load = True
+    #         elif i == "g":
+    #             obj = self.greenSphere
+    #             tex = self.greenTex
+    #             scale = parameters["sphereScale"]
+    #             z=parameters["sphereZ"]
+    #
+    #             plotColor="g"
+    #             plotMarker="o"
+    #             load = True
+    #         else:
+    #             obj = None
+    #             tex = Nonesssss
+    #             scale = None
+    #             z=None
+    #
+    #             plotColor=None
+    #             plotMarker=None
+    #             load = False
+    #
+    #         if j == 0:
+    #             self.evenObj = obj
+    #             self.evenTex = tex
+    #             self.evenScale = scale
+    #             self.evenZ=z
+    #
+    #             self.evenPlotColor=plotColor
+    #             self.evenPlotMarker=plotMarker
+    #             self.evenLoad = load
+    #         else:
+    #             self.oddObj = obj
+    #             self.oddTex = tex
+    #             self.oddScale = scale
+    #             self.oddZ=z
+    #
+    #             self.oddPlotColor=plotColor
+    #             self.oddPlotMarker=plotMarker
+    #             self.oddLoad = load
+    #         j += 1
 
-            1st letter is even instance
-            2nd letter is odd instance
+    # def oddInstance(self, dummy, scale, tex,z):
+    #     dummy.setPos(tuple(parameters["origin"]))
+    #     dummy.setScale(scale)
+    #     dummy.setTexture(tex)
+    #
+    #     for i in range(self.odd.shape[0]):
+    #         self.oddPlaceholder = self.render.attach_new_node("odd Holder")
+    #         self.oddPlaceholder.setPos(self.odd[i][0], self.odd[i][1], z)
+    #         dummy.instanceTo(self.oddPlaceholder)
+    #
+    # def evenInstance(self, dummy, scale, tex,z):
+    #     dummy.setPos(tuple(parameters["origin"]))
+    #     dummy.setScale(scale)
+    #     dummy.setTexture(tex)
+    #
+    #     for i in range(self.even.shape[0]):
+    #         evenPlaceholder = self.render.attach_new_node("even Holder")
+    #         evenPlaceholder.setPos(self.even[i][0], self.even[i][1], z)
+    #         dummy.instanceTo(evenPlaceholder)
 
-        Returns:
-
-        """
-        if len(loadingString) > 2:
-            print "only 2 objects in world"
-
-        j = 0
-        for i in loadingString:
-
-            if i == "t":
-                obj = self.tree
-                tex = self.treeTex
-                scale = parameters["treeScale"]
-                z=parameters["treeZ"]
-
-                plotColor="k"
-                plotMarker="s"
-                load = True
-
-            elif i == "r":
-                obj = self.redSphere
-                tex = self.redTex
-                scale = parameters["sphereScale"]
-                z=parameters["sphereZ"]
-
-                plotColor="r"
-                plotMarker="o"
-                load = True
-            elif i == "g":
-                obj = self.greenSphere
-                tex = self.greenTex
-                scale = parameters["sphereScale"]
-                z=parameters["sphereZ"]
-
-                plotColor="g"
-                plotMarker="o"
-                load = True
+    def getObjects(self):
+        if len(parameters['loadingString']) == 2:
+            if parameters['loadingString'][0] == "1":
+                self.obj1 = self.loader.loadModel(parameters["spherePath"])
+                self.greenTex = self.loader.loadTexture(parameters["greenTexPath"])
+                self.obj1.setTexture(self.greenTex)
+                self.obj1.setScale(parameters['sphereScale'])
             else:
-                obj = None
-                tex = None
-                scale = None
-                z=None
-
-                plotColor=None
-                plotMarker=None
-                load = False
-
-            if j == 0:
-                self.evenObj = obj
-                self.evenTex = tex
-                self.evenScale = scale
-                self.evenZ=z
-
-                self.evenPlotColor=plotColor
-                self.evenPlotMarker=plotMarker
-                self.evenLoad = load
+                self.obj1 = None
+                print "obj1 None"
+            if  parameters['loadingString'][1] == "1":
+                self.obj2 = self.loader.loadModel(parameters["treePath"])
+                self.redTex = self.loader.loadTexture(parameters["redTexPath"])
+                self.obj2.setTexture(self.redTex)
+                self.obj2.setScale(parameters['treeScale'])
             else:
-                self.oddObj = obj
-                self.oddTex = tex
-                self.oddScale = scale
-                self.oddZ=z
+                self.obj2 = None
+                print "obj2 None"
+        else: #  parameters['loadingString'] == "circ":
+            self.obj1 = self.loader.loadModel(parameters["spherePath"])
+            self.greenTex = self.loader.loadTexture(parameters["greenTexPath"])
+            self.obj1.setTexture(self.greenTex)
+            self.obj1.setScale(parameters['sphereScale'])
+            print "circ successfull"
 
-                self.oddPlotColor=plotColor
-                self.oddPlotMarker=plotMarker
-                self.oddLoad = load
-            j += 1
+    def setObjPositions(self):
+        if len(parameters['loadingString']) == 2:
+            self.setTwoPos(self.obj1, self.obj2)
+            print "setTwoPos successful"
+        elif parameters['loadingString'] == "circ":
+            self.setCircPos(obj=self.obj1, teta=0, r=50)
 
-    def oddInstance(self, dummy, scale, tex,z):
-        dummy.setPos(tuple(parameters["origin"]))
-        dummy.setScale(scale)
-        dummy.setTexture(tex)
+    def setTwoPos(self, obj1, obj2):
+        offset = ((int(parameters["worldSize"]) - 1) / 2) + 1
 
-        for i in range(self.odd.shape[0]):
-            self.oddPlaceholder = self.render.attach_new_node("odd Holder")
-            self.oddPlaceholder.setPos(self.odd[i][0], self.odd[i][1], z)
-            dummy.instanceTo(self.oddPlaceholder)
+        quad3PosL=parameters["posL"]
+        quad3PosR=parameters["posR"]
 
-    def evenInstance(self, dummy, scale, tex,z):
-        dummy.setPos(tuple(parameters["origin"]))
-        dummy.setScale(scale)
-        dummy.setTexture(tex)
+        quad4PosL=(parameters["posL"][0]+offset,parameters["posL"][1])
+        quad4PosR=(parameters["posR"][0]+offset,parameters["posR"][1])
 
-        for i in range(self.even.shape[0]):
-            evenPlaceholder = self.render.attach_new_node("even Holder")
-            evenPlaceholder.setPos(self.even[i][0], self.even[i][1], z)
-            dummy.instanceTo(evenPlaceholder)
+        quad2PosL=(parameters["posL"][0],parameters["posL"][1]+offset)
+        quad2PosR=(parameters["posR"][0],parameters["posR"][1]+offset)
+
+        quad1PosL=(parameters["posL"][0]+offset,parameters["posL"][1]+offset)
+        quad1PosR=(parameters["posR"][0]+offset,parameters["posR"][1]+offset)
+
+        self.pos1 = np.array([quad1PosR,quad2PosL,quad3PosL,quad3PosR])
+        self.pos2 = np.array([quad1PosL,quad2PosR,quad4PosL,quad4PosR])
+        if not obj1:
+            pass
+        else:
+            self.obj1.setPos(tuple(parameters["origin"]))
+            for i in range(self.pos1.shape[0]):
+                placeholder1 = self.render.attach_new_node("holder1")
+                placeholder1.setPos(self.pos1[i][0], self.pos1[i][1], parameters["sphereZ"])
+                obj1.instanceTo(placeholder1)
+
+        if not obj2:
+            pass
+        else:
+            self.obj2.setPos(tuple(parameters["origin"]))
+            for i in range(self.pos2.shape[0]):
+                placeholder2 = self.render.attach_new_node("holder2")
+                placeholder2.setPos(self.pos2[i][0], self.pos2[i][1], parameters["treeZ"])
+                obj2.instanceTo(placeholder2)
 
 
+    def setCircPos(self, obj, teta, r):
+        x = parameters['playerInitPos'][0] + (math.sin(math.radians(teta))*r)
+        y = parameters['playerInitPos'][1] + (math.cos(math.radians(teta))*r)
+        pos = (x,y,parameters["sphereZ"])
+        obj.setPos(tuple(parameters['origin']))
+        placeholder = self.render.attach_new_node("holder")
+        placeholder.setPos(pos)
+        obj.instanceTo(placeholder)
 
     def plotPositions(self):
         self.tree=self.greenSphere=self.redSphere=self.treeTex=self.greenTex=self.redTex=None
         self.loadingStringParser(parameters["loadingString"])
         plt.scatter(self.odd[:,0],self.odd[:,1],color=self.oddPlotColor,marker=self.oddPlotMarker,s=80)
         plt.scatter(self.even[:,0],self.even[:,1],color=self.evenPlotColor,marker=self.evenPlotMarker,s=80)#,marker='|',color='g')
-
 
 
     def worldNameGen(self):
